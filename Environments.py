8342fa39c51f9f4c504e77ae6e74e9ede5979a45
wh1ter0se <camelCaseEverything@gmail.com>
2022-02-19 20:31:35 -0600

    Bug fix.

--- Environments.py
+++ Environments.py
@@ -3,6 +3,7 @@ import numpy as np
 class Strip():
     # Lengths should be a list of ints in order to support brdiges in the strip
     def __init__(self, label, lengths:list, port:int, color_order:str='RGB'):
+        self.label = label
         self.port = port
         self.lengths = lengths
         self.length = sum(lengths)
@@ -63,7 +64,7 @@ class House():
                 self.rooms[room.label] = room
 
     def add_room(self,room):
-        self.rooms.append(room)
+        self.rooms[room.label] = room
 
     def get_room(self,room_label) -> Room: 
         if room_label in self.rooms.keys():
@@ -71,14 +72,14 @@ class House():
 
 StateHouse = House()
 StateHouse.add_room(Room('Bedroom'))
-StateHouse.get_room('Bedroom').add_strips(
+StateHouse.get_room('Bedroom').add_strips([
     Strip('SplashA',[20,9],port=0,color_order='RGB'),
     Strip('SplashB',[20,7,20],port=1,color_order='RGB'),
     Strip('SplashC',[20,7],port=3,color_order='RGB'),
     Strip('SplashD',[16,7],port=4,color_order='RGB'),
     Strip('SplashE',[16,7],port=5,color_order='RGB'),
     Strip('Desk',[16,6,16],port=2,color_order='RGB'),
-    Strip('Halo',[44],color_order='RGB'))
+    Strip('Halo',[44],port=7,color_order='RGB')])
 
 
 curr_room = StateHouse.get_room('Bedroom')
