598e5a9446ffa0b83c32159308ca1661ce58de7d
wh1ter0se <camelCaseEverything@gmail.com>
2022-03-23 20:03:46 -0500

    Added intensity modifier.

--- utils/Color.py
+++ utils/Color.py
@@ -2,6 +2,8 @@ import math
 
 default_color_bits = 8 # 8 bit color (0-255)
 
+clamp = lambda n, minn, maxn: max(min(maxn, n), minn)
+
 def hsv2rgb(h,s,v,color_bits=None):
     if color_bits is None: color_bits = default_color_bits
     h = float(h)*(360.0/((2**color_bits)-1)) # convert 0-res to 0-360
@@ -32,8 +34,13 @@ def hsi2rgb(H:float, S:float, I:float):
     #int r, g, b
     H %= 360.0 # cycle H around to 0-360 degrees
     H = 3.14159*H/180.0 # Convert to radians.
-    S = (S if S<1 else 1) if S>0 else 0 # clamp S and I to interval [0,1]
-    I = (I if I<1 else 1) if I>0 else 0
+    # S = (S if S<1 else 1) if S>0 else 0 # clamp S and I to interval [0,1]
+    # I = (I if I<1 else 1) if I>0 else 0
+    S = clamp(S,0,1)
+    I = clamp(I,0,1)
+
+    I_mod = 0.85
+    I /= I_mod
 
     # Math! Thanks in part to Kyle Miller.
     if (H < 2.09439):
@@ -50,4 +57,4 @@ def hsi2rgb(H:float, S:float, I:float):
         b = 255*I/3*(1+S*math.cos(H)/math.cos(1.047196667-H))
         r = 255*I/3*(1+S*(1-math.cos(H)/math.cos(1.047196667-H)))
         g = 255*I/3*(1-S)
-    return r, g, b
+    return int(clamp(r,0,255)), int(clamp(g,0,255)), int(clamp(b,0,255))
