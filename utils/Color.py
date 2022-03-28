33aeb57bd3f8f043ef169a4d2e0a35f7af2b1aca
wh1ter0se <camelCaseEverything@gmail.com>
2022-03-27 19:25:12 -0500

    Restored HSV.

--- utils/Color.py
+++ utils/Color.py
@@ -9,26 +9,26 @@ def hsv2rgb(h,s,v,color_bits=None):
     h = float(h)*(360.0/((2**color_bits)-1)) # convert 0-res to 0-360
     s = float(s) # 0-1.0
     v = float(v) # 0-1.0
-    return hsi2rgb(h,s,v)
-    # h60 = h / 60.0
-    # h60f = math.floor(h60)
-    # hi = int(h60f) % 6
-    # f = h60 - h60f
-    # p = v * (1 - s)
-    # q = v * (1 - f * s)
-    # t = v * (1 - (1 - f) * s)
-    # r, g, b = 0, 0, 0
-    # if hi == 0: r, g, b = v, t, p
-    # elif hi == 1: r, g, b = q, v, p
-    # elif hi == 2: r, g, b = p, v, t
-    # elif hi == 3: r, g, b = p, q, v
-    # elif hi == 4: r, g, b = t, p, v
-    # elif hi == 5: r, g, b = v, p, q
-    # if color_bits is None:
-    #     color_bits = default_color_bits
-    # res = 2**color_bits
-    # r, g, b = int(r*(res-1)), int(g*(res-1)), int(b*(res-1))
-    # return r, g, b
+    # return hsi2rgb(h,s,v)
+    h60 = h / 60.0
+    h60f = math.floor(h60)
+    hi = int(h60f) % 6
+    f = h60 - h60f
+    p = v * (1 - s)
+    q = v * (1 - f * s)
+    t = v * (1 - (1 - f) * s)
+    r, g, b = 0, 0, 0
+    if hi == 0: r, g, b = v, t, p
+    elif hi == 1: r, g, b = q, v, p
+    elif hi == 2: r, g, b = p, v, t
+    elif hi == 3: r, g, b = p, q, v
+    elif hi == 4: r, g, b = t, p, v
+    elif hi == 5: r, g, b = v, p, q
+    if color_bits is None:
+        color_bits = default_color_bits
+    res = 2**color_bits
+    r, g, b = int(r*(res-1)), int(g*(res-1)), int(b*(res-1))
+    return r, g, b
 
 def hsi2rgb(H:float, S:float, I:float):
     #int r, g, b
